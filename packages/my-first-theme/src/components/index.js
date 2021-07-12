2bb00f0f28b1cd81683d007435add3db58b96c65
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-07-12 09:30:50 -0700

    Link and Switch

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
 
 const Root = ({state}) => {
+    const data = state.source.get(state.router.link); 
     return (
       <>
         <h1>Hello Frontity</h1>
         <p>Current URL: {state.router.link}</p>
+        <nav>
+            <Link link="/">Home</Link>
+            <br />
+            <Link link="/page/2">More Posts</Link>
+            <br />
+            <Link link="/about-us">About-Us</Link>
+            <br />
+        </nav>
+        <hr /> 
+        <main>
+            <Switch>
+                <div when={data.isArchive}>This is a list</div>
+                <div when={data.isPost}>This is a post</div>
+                <div when={data.isPage}>This is a page</div>
+            </Switch>
+        </main>
       </>
     );
   };
